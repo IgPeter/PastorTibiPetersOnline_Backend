9a53bc37af0adb06fd89f69b41e8937bcd6920ed
Peter Igaga <igagapeter477@gmail.com>
2026-02-26 16:01:48 +0100

    updated dist

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(`${api}/user`, userRouter);
 app.use(`${api}/category`, categoryRouter);
 app.use(`${api}/subscription`, subscriptionRouter);
 app.use(`${api}/paystack`, paystackRouter);
-app.use(`${api}/spreadTheWord`, spreadTheWordRouter);
+app.use(`${api}/spreadtheWord`, spreadTheWordRouter);
 
 //connect to database
 mongoose
